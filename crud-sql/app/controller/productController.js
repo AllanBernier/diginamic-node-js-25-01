77e7750a430b4200ade698303076d4771703fc5f
Allan <allan.bernier1@gmail.com>
2025-01-06 11:24:24 +0100

    CRUD SQL Product

--- crud-sql/app/controller/productController.js
+++ crud-sql/app/controller/productController.js
@@ -3,29 +3,37 @@ const Product = require("../model/Product")
 const getById = (req, res) => {
   const { id } = req.params
 
+  Product.findByPk(parseInt(id)).then((product) => {
+    res.json(product)
+  })
 }
 
 const getAll = (req, res) => {
+
+  Product.findAll().then((products) => {
+    res.json(products)
+  })
+
 }
 
 const create = (req, res) => {
-  Product.create({ 
-    name: "toto",
-    price : 152,
-    description : "Hello"
-  }).then( (result) => {
+  Product.create(req.body).then((result) => {
     res.status(201).json(result)
   })
 }
 
 const update = (req, res) => {
   const { id } = req.params
-  const product = {... req.body, id:parseInt(id)  }
+
+  Product.update(req.body, { where: { id: parseInt(id) } }).then((product) => {
+    res.json({ message: "Product updated", product })
+  })
 }
 
-
 const destroy = (req, res) => {
-  const { id } = req.params
+  Product.destroy({ where: { id: parseInt(req.params.id) } }).then((product) => {
+    res.json({ message: "Product deleted", product })
+  })
 }
 
 module.exports = { getById, getAll, create, update, destroy }
