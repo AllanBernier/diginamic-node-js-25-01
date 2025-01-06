ab3b9436ecdd75f0c100dc660813d1b0a68d84f5
Allan <allan.bernier1@gmail.com>
2025-01-06 14:15:48 +0100

    User Sequelize

--- user-sql/app/controller/userController.js
+++ user-sql/app/controller/userController.js
@@ -1,9 +1,80 @@
 const User = require("../model/User")
 
+
+const getById = (req, res) => {
+  const { id } = req.params
+  User.findByPk(parseInt(id))
+    .then((u) => {
+      if (!u) {
+        return res.status(404).json({ message: "User not found !" })
+      }
+      res.json(u)
+    }).catch((err) => {
+      res.status(500).json({ message: "Internal server error !" })
+    })
+}
+
+const index = async (req, res) => {
+  const page = parseInt(req.query.page) || 1
+  const limit = parseInt(req.query.page) || 10
+  const offset = (page - 1) * limit
+
+  try {
+    const result = await User.findAndCountAll({ limit, offset })
+
+    console.log(result)
+    res.json({
+      data: result.rows,
+      total: result.count,
+      currentPage: page,
+      totalPages: Math.round(result.count / limit)
+    })
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" })
+  }
+}
+
+
+const update = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const user = await User.findByPk(parseInt(id))
+    if (!user) {
+      return res.status(404).json({ message: "User not found !" })
+    }
+
+    await user.update(req.body)
+    res.json({message : "User updated successfully !"})
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" })
+  }
+
+}
+
+const destroy = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const user = await User.findByPk(parseInt(id))
+    if (!user) {
+      return res.status(404).json({ message: "User not found !" })
+    }
+
+    await user.destroy()
+    
+    res.json({message : "User destroyd successfully !"})
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" })
+  }
+}
+
+
 const create = async (req, res) => {
   const user = await User.create(req.body)
   res.status(201).json(user)
 }
 
 
-module.exports = { create }
+
+module.exports = { create, getById, index, update, destroy }
--- user-sql/app/router/userRouter.js
+++ user-sql/app/router/userRouter.js
@@ -1,8 +1,11 @@
-const { create } = require("../controller/userController")
+const { create, getById, index, destroy, update } = require("../controller/userController")
 const express = require("express")
 const router = express.Router()
 
 router.post("/users", create)
-
+router.get("/users", index)
+router.get("/users/:id", getById)
+router.put("/users/:id", update)
+router.delete("/users/:id", destroy)
 
 module.exports = router
